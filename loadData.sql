e6e73da0c2055b7fc6e18351f73621b4a3231517
Matías Figueroa Contreras <matias.figueroa.c@usach.cl>
2023-04-01 19:36:43 -0400

    [Matias Figueroa] Creación de datos dummy para alumno y curso + corrección apoderado

--- loadData.sql
+++ loadData.sql
@@ -1,34 +1,82 @@
 INSERT INTO public.comuna (nombre)
-VALUES
-    ('Santiago'),
-    ('San Ramon'),
-    ('Estacion Central'),
-    ('La Cisterna'),
-    ('La Pintana'),
-    ('Providencia'),
-    ('Cerrillos'),
-    ('El Bosque'),
-    ('Maipu'),
-    ('Las Condes'),
-    ('La Florida'),
-    ('San Miguel'),
-    ('Pedro Aguirre Cerda'),
-    ('Cerrillos'),
-    ('Renca'),
-    ('La Granja'),
-    ('Recoleta'),
-    ('La Reina'),
-    ('Lo Espejo'),
-    ('Vitacura');
+VALUES ('Santiago'), 
+       ('San Ramon'), 
+       ('Estacion Central'), 
+       ('La Cisterna'), 
+       ('La Pintana'), 
+       ('Providencia'), 
+       ('Cerrillos'), 
+       ('El Bosque'), 
+       ('Maipu'), 
+       ('Las Condes'), 
+       ('La Florida'), 
+       ('San Miguel'), 
+       ('Pedro Aguirre Cerda'), 
+       ('Cerrillos'), 
+       ('Renca'), 
+       ('La Granja'), 
+       ('Recoleta'), 
+       ('La Reina'), 
+       ('Lo Espejo'), 
+       ('Vitacura');
+
 
 INSERT INTO public.colegio (id_comuna, nombre)
-SELECT trunc((random()*19) + 1), 'Colegio ' || generate_series(1, 100) as name;
+SELECT trunc((random()*19) + 1),
+       'Colegio ' || generate_series(1, 100) as name;
 
-WITH parentezco_list as (
-	SELECT '{padre, madre, abuelo, abuela, tio, hermano}'::VARCHAR(50)[] parentezco
-), sexo_list as (
-	SELECT '{hombre, mujer}'::VARCHAR(50)[] sexo
-)
+WITH parentezco_list as
+    (SELECT '{padre, madre, abuelo, abuela, tio, tia}'::VARCHAR(50)[] parentezco),
+     sexo_list as
+    (SELECT '{mujer, hombre}'::VARCHAR(50)[] sexo),
+     series as
+    (SELECT generate_series(1, 100) as n,
+            MOD(cast(trunc(random()*6) as int), 6) + 1 as random)
 INSERT INTO public.apoderado (id_comuna, nombre, parentezco, sexo, edad)
-SELECT trunc((random()*19) + 1), 'Apoderado ' || generate_series(1, 100) as name, parentezco[MOD(cast(trunc(random()*6) as int), 6) + 1], sexo[MOD(cast(trunc(random()*2) as int), 2) + 1], cast(trunc(random()*40 + 20) as int)
-FROM parentezco_list, sexo_list;
+SELECT trunc((random()*19) + 1),
+       'Apoderado ' || series.n as name,
+       parentezco[series.random],
+       sexo[MOD(series.random, 2) + 1],
+       cast(trunc(random()*40 + 20) as int)
+FROM parentezco_list,
+     sexo_list,
+     series;
+
+WITH apoderado_colegio_list as
+    (SELECT a.id_apoderado,
+            a.id_comuna,
+            c.id_colegio,
+            ROW_NUMBER() OVER (
+                               ORDER BY random()) as row_number
+     FROM apoderado a,
+          colegio c
+     WHERE c.id_comuna = a.id_comuna
+     ORDER BY random()
+     LIMIT 150),
+     sexo_list as
+    (SELECT '{mujer, hombre}'::VARCHAR(50)[] sexo)
+INSERT INTO public.alumno (id_apoderado, id_comuna, id_colegio, edad, sexo, nombre)
+SELECT acl.id_apoderado,
+       acl.id_comuna,
+       acl.id_colegio,
+       cast(trunc(random()*15 + 5) as int),
+       sexo[MOD(cast(trunc(random()*2) as int), 2) + 1],
+       'Alumno ' || acl.row_number as name
+FROM apoderado_colegio_list acl,
+     sexo_list;
+
+
+INSERT INTO public.curso (nombre)
+VALUES ('Lenguaje'), 
+       ('Matematicas'), 
+       ('Ingles'), 
+       ('Religion'), 
+       ('Quimica'), 
+       ('Biologia'), 
+       ('Tecnologia'), 
+       ('Fisica'), 
+       ('Educacion Fisica'), 
+       ('Historia'), 
+       ('Musica'), 
+       ('Artes'),
+       ('Filosofia');
