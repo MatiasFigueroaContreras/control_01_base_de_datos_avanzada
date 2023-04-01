a715d536fa1503612eab995e0bc2ade2b901864d
cristowo <cristobal.marchant@usach.cl>
2023-04-01 02:57:47 -0300

    [Cristóbal Marchant] Arreglo de """""pequeñitos"""" detalles
    Gracias Samoth

--- dbCreate.sql
+++ dbCreate.sql
@@ -1,76 +1,32 @@
 CREATE DATABASE colegiosdb
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
+	WITH
+	OWNER = postgres
+	ENCODING = 'UTF8'
+	LC_COLLATE = 'Spanish_Chile.1252'
+	LC_CTYPE = 'Spanish_Chile.1252'
+	TABLESPACE = pg_default
+	CONNECTION LIMIT = -1
+	IS_TEMPLATE = False;
 
-CREATE TABLE colegiosdb.schemas.sueldo (
-    id_sueldo int NOT NULL,
-    id_empleado int,
-    cantidad int,
-    PRIMARY KEY (id_sueldo)
-    CONSTRAINT
-    FOREIGN KEY (id_empleado) REFERENCES empleado(id_empleado)
-);
+COMMENT ON DATABASE colegiosdb
+	IS 'control 1 base de datos';
 
-CREATE TABLE colegiosdb.schemas.empleado (
-    id_empleado int NOT NULL,
-    id_colegio int,
+-- Tablas de la base de datos
+-- se ponen en el query
+-- lo de arriba no 
+
+CREATE TABLE colegiosdb.schemas.comuna (
+    id_comuna int NOT NULL,
     nombre varchar(50),
-    rol varchar(50),
-    sexo varchar(50),
-    PRIMARY KEY (id_empleado)
-    CONSTRAINT
-        FOREING KEY (id_colegio) REFERENCES sueldo(id_colegio)
-);
-
-CREATE TABLE colegiosdb.schemas.profesor (
-    id_profesor int NOT NULL,
-    id_empleado int,
-    correo varchar(50),
-    PRIMARY KEY (id_profesor)
-    CONSTRAINT
-        FOREING KEY (id_empleado) REFERENCES empleado(id_empleado)
+    PRIMARY KEY (id_comuna)
 );
 
 CREATE TABLE colegiosdb.schemas.colegio (
     id_colegio int NOT NULL,
     id_comuna int,
     nombre varchar(50) NOT NULL,
-    PRIMARY KEY (id_colegio)
-    CONSTRAINT
-        FOREING KEY (id_comuna) REFERENCES comuna(id_comuna)
-);
-
-CREATE TABLE colegiosdb.schemas.prof_curso (
-    id_prof_curso int NOT NULL,
-    id_profesor int,
-    id_curso int,
-    jefe BOOLEAN,
-    PRIMARY KEY (id_prof_curso)
-    CONSTRAINT
-        FOREING KEY (id_profesor) REFERENCES profesor(id_profesor)
-        FOREING KEY (id_curso) REFERENCES curso(id_curso)
-);
-
-CREATE TABLE colegiosdb.schemas.franja_horaria (
-    id_frangah int NOT NULL,
-    cantidad_horas int,
-    PRIMARY KEY (id_frangah)
-);
-
-CREATE TABLE colegiosdb.schemas.asistencia (
-    id_asistencia int NOT NULL,
-    cantidad int,
-    ano date,
-    PRIMARY KEY (id_asistencia)
-);
-
-CREATE TABLE colegiosdb.schemas.comuna (
-    id_comuna int NOT NULL,
-    nombre varchar(50),
-    PRIMARY KEY (id_comuna)
+    PRIMARY KEY (id_colegio),
+    CONSTRAINT fk_colegio_comuna FOREIGN KEY (id_comuna) REFERENCES colegiosdb.schemas.comuna(id_comuna)
 );
 
 CREATE TABLE colegiosdb.schemas.curso (
@@ -79,16 +35,22 @@ CREATE TABLE colegiosdb.schemas.curso (
     PRIMARY KEY (id_curso)
 );
 
-CREATE TABLE colegiosdb.schemas.alu_curso (
-    id_alu_curso int NOT NULL,
-    id_alumno int,
+CREATE TABLE colegiosdb.schemas.plantilla_curso (
+    id_plantilla_curso int NOT NULL,
     id_curso int,
-    id_asistencia int,
-    PRIMARY KEY (id_alu_curso)
-    CONSTRAINT
-        FOREING KEY (id_alumno) REFERENCES alumno(id_alumno)
-        FOREING KEY (id_curso) REFERENCES curso(id_curso)
-        FOREING KEY (id_asistencia) REFERENCES asistencia(id_asistencia)
+    PRIMARY KEY (id_plantilla_curso),
+    CONSTRAINT fk_plantilla_curso_curso
+    	FOREIGN KEY (id_curso) REFERENCES colegiosdb.schemas.curso(id_curso)
+);
+
+CREATE TABLE colegiosdb.schemas.apoderado (
+    id_apoderado int NOT NULL,
+    id_comuna int,
+    parentezco varchar(50),
+    sexo varchar(50),
+    edad int,
+    PRIMARY KEY (id_apoderado),
+    FOREIGN KEY (id_comuna) REFERENCES colegiosdb.schemas.comuna(id_comuna)
 );
 
 CREATE TABLE colegiosdb.schemas.alumno (
@@ -99,28 +61,70 @@ CREATE TABLE colegiosdb.schemas.alumno (
     edad int,
     sexo varchar(50),
     nombre varchar(50),
-    PRIMARY KEY (id_alumno)
-    CONSTRAINT
-        FOREING KEY (id_apoderado) REFERENCES apoderado(id_apoderado)
-        FOREING KEY (id_comuna) REFERENCES comuna(id_comuna)
-        FOREING KEY (id_colegio) REFERENCES colegio(id_colegio)
+    PRIMARY KEY (id_alumno),
+    FOREIGN KEY (id_apoderado) REFERENCES colegiosdb.schemas.apoderado(id_apoderado),
+    FOREIGN KEY (id_comuna) REFERENCES colegiosdb.schemas.comuna(id_comuna),
+    FOREIGN KEY (id_colegio) REFERENCES colegiosdb.schemas.colegio(id_colegio)
 );
 
-CREATE TABLE colegiosdb.schemas.plantilla_curso (
-    id_plantilla_curso int NOT NULL,
+CREATE TABLE colegiosdb.schemas.asistencia (
+    id_asistencia int NOT NULL,
+    cantidad int,
+    ano date,
+    PRIMARY KEY (id_asistencia)
+);
+
+CREATE TABLE colegiosdb.schemas.alu_curso (
+    id_alu_curso int NOT NULL,
+    id_alumno int,
     id_curso int,
-    PRIMARY KEY (id_plantilla_curso)
-    CONSTRAINT
-        FOREING KEY (id_curso) REFERENCES curso(id_curso)
+    id_asistencia int,
+    PRIMARY KEY (id_alu_curso),
+    CONSTRAINT fk_alu_curso_alumno FOREIGN KEY (id_alumno) REFERENCES colegiosdb.schemas.alumno(id_alumno),
+    CONSTRAINT fk_alu_curso_curso FOREIGN KEY (id_curso) REFERENCES colegiosdb.schemas.curso(id_curso),
+    CONSTRAINT fk_alu_curso_asistencia FOREIGN KEY (id_asistencia) REFERENCES colegiosdb.schemas.asistencia(id_asistencia)
 );
 
-CREATE TABLE colegiosdb.schemas.apoderado (
-    id_apoderado int NOT NULL,
-    id_comuna int,
-    parentezco varchar(50),
+CREATE TABLE colegiosdb.schemas.empleado (
+    id_empleado int NOT NULL,
+    id_colegio int,
+    nombre varchar(50),
+    rol varchar(50),
     sexo varchar(50),
-    edad int,
-    PRIMARY KEY (id_apoderado)
-    CONSTRAINT
-        FOREING KEY (id_comuna) REFERENCES comuna(id_comuna)
+    PRIMARY KEY (id_empleado),
+    CONSTRAINT fk_empleado_colegio FOREIGN KEY (id_colegio) REFERENCES colegiosdb.schemas.colegio(id_colegio)
+);
+
+CREATE TABLE colegiosdb.schemas.sueldo (
+    id_sueldo int NOT NULL,
+    id_empleado int,
+    cantidad int,
+    PRIMARY KEY (id_sueldo),
+    CONSTRAINT fk_sueldo_empleado FOREIGN KEY (id_empleado) REFERENCES colegiosdb.schemas.empleado(id_empleado)
+);
+
+CREATE TABLE colegiosdb.schemas.profesor (
+    id_profesor int NOT NULL,
+    id_empleado int,
+    correo varchar(50),
+    PRIMARY KEY (id_profesor),
+    CONSTRAINT fk_profesor_empleado FOREIGN KEY (id_empleado) REFERENCES colegiosdb.schemas.empleado(id_empleado)
+);
+
+CREATE TABLE colegiosdb.schemas.franja_horaria (
+    id_franja_horaria int NOT NULL,
+    cantidad_horas int,
+    PRIMARY KEY (id_franja_horaria)
+);
+
+CREATE TABLE colegiosdb.schemas.prof_curso (
+    id_prof_curso int NOT NULL,
+    id_profesor int,
+    id_curso int,
+	id_franja_horaria int,
+    jefe BOOLEAN,
+    PRIMARY KEY (id_prof_curso),
+    CONSTRAINT fk_prof_curso_profesor FOREIGN KEY (id_profesor) REFERENCES colegiosdb.schemas.profesor(id_profesor),
+    CONSTRAINT fk_prof_curso_curso FOREIGN KEY (id_curso) REFERENCES colegiosdb.schemas.curso(id_curso),
+	CONSTRAINT fk_prof_curso_franja_horaria FOREIGN KEY (id_franja_horaria) REFERENCES colegiosdb.schemas.franja_horaria(id_franja_horaria)
 );
