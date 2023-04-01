2b9a7fde10004c1e92c24794b458360af412c43e
diego oliva <diego.oliva@usach.cl>
2023-04-01 03:13:45 -0300

    ['Diego Oliva'] arreglo de pequeños detalles complementarios

--- dbCreate.sql
+++ dbCreate.sql
@@ -1,19 +1,24 @@
-CREATE DATABASE colegiosdb
-	WITH
-	OWNER = postgres
-	ENCODING = 'UTF8'
-	LC_COLLATE = 'Spanish_Chile.1252'
-	LC_CTYPE = 'Spanish_Chile.1252'
-	TABLESPACE = pg_default
-	CONNECTION LIMIT = -1
-	IS_TEMPLATE = False;
+-- Crear database
+CREATE DATABASE colegiosdb 
+	WITH 
+	OWNER = postgres 
+	ENCODING = 'UTF8' 
+	LC_COLLATE = 'Spanish_Chile.1252' 
+	LC_CTYPE = 'Spanish_Chile.1252' 
+	TABLESPACE = pg_default 
+	CONNECTION LIMIT = -1 
+	IS_TEMPLATE = False; 
+ 
+COMMENT ON DATABASE colegiosdb 
+	IS 'control 1 base de datos';}
+	
+-- Creacion del esquema	
+CREATE SCHEMA schemas;
 
-COMMENT ON DATABASE colegiosdb
-	IS 'control 1 base de datos';
-
--- Tablas de la base de datos
--- se ponen en el query
--- lo de arriba no 
+-- Tablas de la base de datos 
+-- se ponen en el query 
+-- lo de arriba no  
+ 
 
 CREATE TABLE colegiosdb.schemas.comuna (
     id_comuna int NOT NULL,
