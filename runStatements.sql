a92b230a7d67ba6a6d2e2cbb408335893700796d
Juan R <juan.ramirez.m@usach.cl>
2023-04-02 05:34:51 -0400

    [Juan Ramirez] Creacion de la query 3 y 4

--- runStatements.sql
+++ runStatements.sql
@@ -19,3 +19,18 @@ WHERE e.id_empleado = p.id_empleado and s.id_empleado = e.id_empleado and pc.id_
 SELECT a.nombre, 22-asi.cantidad as inasistencias, asi.mes, asi.anio
 FROM alumno a, asistencia asi, alu_curso ac
 WHERE a.id_alumno = ac.id_alumno and ac.id_asistencia = asi.id_asistencia and asi.anio = 2019;
+
+--Query 3
+--Lista de empleados con su rol, sueldo y comuna de residencia ordenado por comuna y sueldo.
+SELECT e.nombre, e.rol, s.cantidad, c.nombre as comuna
+FROM empleado e, sueldo s, comuna c, colegio co
+WHERE e.id_empleado = s.id_empleado and co.id_comuna = c.id_comuna and e.id_colegio = co.id_colegio
+ORDER BY c.nombre desc, s.cantidad;
+
+--Query 4
+--Lista de cursos que cuentan con la menor cantidad de alumnos y el año en que se dicta.
+SELECT cu.nombre as nombre_curso, COUNT(*) as cantidad_alumnos, asi.anio
+FROM alu_curso ac, asistencia asi, curso cu
+WHERE asi.id_asistencia = ac.id_asistencia and cu.id_curso = ac.id_curso
+GROUP BY cu.nombre, asi.anio
+ORDER BY cantidad_alumnos asc;
